1e5b96df1f8ffe8f97c019bbb6144be9d83262b9
somq14 <som.q14@gmail.com>
2022-03-19 21:54:56 +0900

    ♻️ replace hard coded version

--- build.sh
+++ build.sh
@@ -2,14 +2,14 @@
 
 docker build \
   -t devcontainer:latest \
-  --build-arg DEBIAN_VERSION=11.2 \
+  --build-arg DEBIAN_VERSION=${DEBIAN_VERSION} \
   --build-arg USER_NAME=vscode \
   --build-arg USER_UID=$(id -u) \
   --build-arg USER_GID=$(id -g) \
-  --build-arg PYTHON_VERSION=3.10.2 \
-  --build-arg NODEJS_VERSION=14.18.3 \
-  --build-arg YARN_VERSION=1.22.17 \
-  --build-arg AWS_CLI_VERSION=2.4.19 \
-  --build-arg AWS_MFA_VERSION=0.0.12 \
-  --build-arg AWS_SAM_VERSION=1.38.1 \
+  --build-arg PYTHON_VERSION=${PYTHON_VERSION} \
+  --build-arg NODEJS_VERSION=${NODEJS_VERSION} \
+  --build-arg YARN_VERSION=${YARN_VERSION} \
+  --build-arg AWS_CLI_VERSION=${AWS_CLI_VERSION} \
+  --build-arg AWS_MFA_VERSION=${AWS_MFA_VERSION} \
+  --build-arg AWS_SAM_VERSION=${AWS_SAM_VERSION} \
   .
